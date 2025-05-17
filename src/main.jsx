16e8c02871f96d4e5a1533bedeb6efe062c68c84
ErickSo5 <ericksolorzanolopez@gmail.com>
2025-05-17 15:41:06 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './App.css'
 import App from './App.jsx'
+import imagenDeBlog from './images/imagen-de-blog.png';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
