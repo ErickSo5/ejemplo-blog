c265690b1b7fe489b275ead57102187c7514b5e7
Erick Solorzano <ericksolorzanolopez@gmail.com>
2025-05-17 15:29:18 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return(
+    <div className='cuadro-blog'>
+      <img className='imagen-blog' src='src/images/imagen-de-blog.png' alt='Imgaen de blog'></img>
+      <div className='cuadro-learning'>
+        Leraning
+      </div>
+      <div className='cuadro-publicado'>
+        Published 21 Dec 2023
+      </div>
+      <div className='cuadro-HTML'>
+        HTML & CSS foundations
+      </div>
+      <div className='cuadro-texto'>
+        These languages are the backbone of every website, defeting structure, context, and presentatios
+      </div>
+      <div className='cuadro-perfil'>
+        <img src="src/images/image-avatar.webp" alt="" />
+        <div>
+          Greg Hooper
+        </div>
+      </div>
+    </div>
+
+  )
+  
+}
+
+export default App
